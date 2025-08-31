d077561b19b3237b30c249a828b33b93cde80167
hariswara <hariswaranvenkatesh@gmail.com>
2025-08-31 18:59:35 +0530

    feat: course section ui work

--- client/src/context/AppContextProvider.jsx
+++ client/src/context/AppContextProvider.jsx
@@ -1,10 +1,22 @@
+import { useEffect, useState } from "react";
+import { dummyCourses } from "../assets/assets";
 import { AppContext } from "./AppContext";
 
 export const AppContextProvider = (props) => {
-  const value = {};
+  const rupees = import.meta.env.VITE_RUPEES;
+  const [allCourses, setAllCourses] = useState([]);
+
+  // Fetch All Courses
+  const fetchAllCourses = async () => {
+    setAllCourses(dummyCourses);
+  };
+
+  useEffect(() => {
+    fetchAllCourses();
+  }, []);
+
+  const value = { rupees, allCourses };
   return (
-    <AppContext.Provider value={{ value }}>
-      {props.children}
-    </AppContext.Provider>
+    <AppContext.Provider value={value}>{props.children}</AppContext.Provider>
   );
 };
